b49f16fcbd4a11f21d35b9b4ec85ef0e95168461
Radu Chindris <radu.chindris@devtelsoftware.com>
2023-01-03 20:27:24 +0200

    checking for duplicated tag definitions

--- hyperconf/templates.py
+++ hyperconf/templates.py
@@ -1,5 +1,6 @@
 """Provide experiment configuration templates support."""
 import yaml
+from yaml.loader import SafeLoader
 from pathlib import Path
 from typing import Dict, List
 try:
@@ -8,11 +9,41 @@ except ImportError:
     raise ImportError("Could not find module importlib.resources."
                       "Python versions <3.7 are not supported.")
 
+from hyperconf.errors import TemplateDefinitionError
+
+
+class _LoadLineInfo(SafeLoader):
+    """Adds line numbers to parsed yaml dicts."""
+
+    def construct_mapping(self, node, deep=False):
+        """Augument parsed nodes."""
+        mapping = super().construct_mapping(node, deep=deep)
+        mapping['__line__'] = node.start_mark.line + 1
+        return mapping
+
+
+class ArgumentDefinition:
+    """A configuration element parameter definition."""
+    def __init__(self, node: Dict, path: str | Path):
+        """Initialize from spec.
+
+        Args:
+        node (Dict): a dictionary containing argument
+        definition properties. Any argument must have a
+        name, a type and a default value.
+        """
+        if node is None:
+            raise ValueError("node is None")
+        if "name" not in node:
+            raise TemplateDefinitionError("args",
+                                          node["__line__"],
+                                          path, "Property 'name' is required")
+
 
 class ElementDefinition:
     """Configuration template element definition."""
 
-    def __init__(self, name: str, arguments: Dict = {}):
+    def __init__(self, name: str, node: Dict, template_path: str = None):
         """Initialize an ElementDefinition.
 
         Arguments:
@@ -21,34 +52,41 @@ class ElementDefinition:
         element or not. Default: False.
         argments (List[Dict]): a list of supported arguments. Default: empty.
         """
-        if name is None:
-            raise ValueError("name is None")
-        if arguments is None:
-            raise ValueError("arguments is None")
+        if node is None:
+            raise ValueError("node is None")
 
         self._name = name
-        self._type = arguments.get("type", "str")
-        self._required = arguments.get("required", False)
-
+        self._type = node.get("type", str)
+        self._required = node.get("required", False)
+        self._file = template_path
+        self._line = node.get("__line__")
         self._args = []
 
-        for arg_name, arg_def in arguments.items():
-            if arg_name in ["type", "required"]:
-                continue
+        if "args" in node:
+            arg_defs = node["args"]
+            if not isinstance(arg_defs, list):
+                raise TemplateDefinitionError(
+                    "args",
+                    arg_defs["__line__"], self._file,
+                    "The component definition arguments must be "
+                    "specified as a list.")
+
+        for arg_name, arg_def in node.items():
             if arg_name == "args":
-                for component_def in arg_def:
-                    self._args.append(ElementDefinition(component_def))
-            else:
-                raise ValueError(f"unsupported element definition: {arg_name}:"
-                                 "expecting a 'args' tag.")
+                for arg in arg_def:
+                    self._args.append(ArgumentDefinition(arg, self._file))
 
     def __repr__(self):
-        """Serialize to string."""
+        """Object representation."""
         text = f"[tag: {self._name}, type:{self._type}, args: ("
         for elem in self._args:
             text += str(elem)
         text += ")]"
         return text
+
+    def __str__(self):
+        """Convert to string."""
+        return f"ElementDefinition:{self._name}"
 
     @property
     def name(self):
@@ -59,6 +97,16 @@ class ElementDefinition:
     def type(self):
         """Element type name."""
         return self._type
+
+    @property
+    def file(self):
+        """Definition file."""
+        return self._file
+
+    @property
+    def line_number(self):
+        """Definition line number."""
+        return self._line
 
 
 class ConfigurationDefinitions:
@@ -97,19 +145,7 @@ class ConfigurationDefinitions:
         self._tags = {}
 
         for template_path in template_files:
-            self.load(template_path)
-
-    def _parse(self, template_dict):
-        if "name" not in template_dict:
-            raise ValueError("Invalid template: expecting a 'name' key.")
-
-        for tag_name, content in template_dict.items():
-            if tag_name == "name":
-                self._names.append(content)
-            elif tag_name == "description":
-                self._descriptions.append(content)
-            else:
-                self._tags[tag_name] = ElementDefinition(tag_name, content)
+            self.parse(template_path)
 
     def __repr__(self):
         """Object representation."""
@@ -119,7 +155,7 @@ class ConfigurationDefinitions:
         """Object to string."""
         return f"ConfigurationTemplate: templates={self._names}"
 
-    def load(self, template_path: str | Path):
+    def parse(self, template_path: str | Path):
         """Load a template file.
 
         Args:
@@ -132,8 +168,8 @@ class ConfigurationDefinitions:
             isinstance(template_path, str) else template_path
 
         if not template_path.exists():
-            # Search for a package resource
-            # with the same name (predefined template).
+            # Search for a packaged resource
+            # having the same name (predefined template).
             pkg_files = resources.files("hyperconf")
             template_path = pkg_files / "templates" / template_path.name
 
@@ -142,13 +178,32 @@ class ConfigurationDefinitions:
                               "Please check that the file exists and is a "
                               "valid configuration template definition file.")
 
-            with open(template_path) as tfile:
-                try:
-                    template_def = yaml.load(tfile, yaml.CLoader)
-                except yaml.scanner.ScannerError as e:
-                    raise IOError(f"Invalid YAML file: {e}")
+        with open(template_path) as tfile:
+            try:
+                template_def = yaml.load(tfile, Loader=_LoadLineInfo)
+            except yaml.scanner.ScannerError as e:
+                raise IOError(f"Invalid YAML file: {e}")
 
-            self._parse(template_def)
+        for tag_name, node in template_def.items():
+            if tag_name == "__line__":
+                continue
+            elif tag_name == "name":
+                self._names.append(node)
+            elif tag_name == "description":
+                self._descriptions.append(node)
+            else:
+                if tag_name in self._tags:
+                    existing_tag = self._tags[tag_name]
+                    raise TemplateDefinitionError(
+                        tag_name,
+                        template_def["__line__"],
+                        template_path,
+                        "Duplicate tag definition. This tag is already "
+                        f"defined in {existing_tag.file} at line "
+                        f"{existing_tag.line_number}"
+                    )
+                self._tags[tag_name] = ElementDefinition(
+                    tag_name, node, template_path)
 
 
 if __name__ == "__main__":
